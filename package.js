e6a44be380e30c793e9681e63c2229427caae674
Rod Cope <rod@perfdog.com>
2019-07-11 08:55:51 -0600

    Revert versionsFrom bump.

--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
   git: 'https://github.com/rodcope1/meteor-collection-behaviours.git',
   name: 'rodcope1:collection-behaviours',
   summary: 'Define and attach behaviours to collections',
-  version: '2.0.0'
+  version: '2.0.1'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.8');
+  api.versionsFrom('1.0');
 
   api.use([
     'check',
