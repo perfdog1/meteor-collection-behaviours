43fb846c36bd9567dacfa0590b85a0a3b1af2b21
Rod Cope <rod@perfdog.com>
2025-03-16 11:09:37 -0600

    Relax 'mongo' version constraint.

--- package.js
+++ package.js
@@ -2,16 +2,16 @@ Package.describe({
   git: 'https://github.com/perfdog1/meteor-collection-behaviours.git',
   name: 'perfdog1:collection-behaviours',
   summary: 'Define and attach behaviours to collections',
-  version: '3.0.1'
+  version: '3.0.2'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('3.0.1');
 
   api.use([
-    'check',
+    'check@1.4.0',
     'coffeescript@2.0.0',
-    'mongo@2.0.0'
+    'mongo@1.16.0 || 2.0.0'
   ]);
 
   api.addFiles([
